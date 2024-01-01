665078d7402894bfd742beae5b0b2efdb1590d62
Mateo Monsalve <mateomonsalve502@gmail.com>
2024-01-01 18:12:29 -0500

    Arreglando error de ejecucción parte 1

--- Sales.WEBAPP/Program.cs
+++ Sales.WEBAPP/Program.cs
@@ -1,11 +1,21 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sales.WEBAPP.Repositories;
 using Sales.WEBAPP;
+using System.Text.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IRepository, Repository>();
+
+//HttpClient
+builder.Services.AddHttpClient<IRepository, Repository>(c => c.BaseAddress = new Uri("https://localhost:7069/"));
+// JsonSerializerOptions
+builder.Services.AddSingleton(sp => new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+//SweetAlert2
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
