16768b1c134fd8de95bfbf7f4b4138173090b0cd
Mateo Monsalve <mateomonsalve502@gmail.com>
2024-01-01 16:08:04 -0500

    Adding Countries methods

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -17,24 +17,47 @@ namespace Sales.API.Controllers
 		}
 
 		[HttpGet]
-		public async Task<ActionResult<IEnumerable<Country>>> GetAllAsync()
+		public async Task<ActionResult<IEnumerable<Country>>> Get()
 		{
 			return Ok(await _context.Countries.ToListAsync());
 		}
 
-		[HttpGet("{id}")]
-		public async Task<ActionResult<Country>> GetByIdAsync(int id)
+		[HttpGet("{id:int}")]
+		public async Task<ActionResult<Country>> GetById(int id)
 		{
-			return Ok(await _context.Countries.FindAsync(id));
+			var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+			return country == null ? NotFound() : Ok(country);
 		}
 
 		[HttpPost]
-		public async Task<ActionResult<Country>> PostAsync(Country country)
+		public async Task<ActionResult<Country>> Add(Country country)
 		{
 			_context.Add(country);
 			await _context.SaveChangesAsync();
 
 			return Ok(country);
+		}
+
+		[HttpPut]
+		public async Task<ActionResult<Country>> Update(Country country)
+		{
+			_context.Update(country);
+			await _context.SaveChangesAsync();
+
+			return Ok(country);
+		}
+
+		[HttpDelete("{id:int}")]
+		public async Task<ActionResult> Delete(int id)
+		{
+			var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+			
+			if (country == null) return NotFound();
+
+			_context.Remove(country);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
 		}
 	}
 }
